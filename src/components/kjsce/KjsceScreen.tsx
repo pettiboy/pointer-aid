08a2e2e0161f2eef6348001efc378d64b3e6ecf9
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2022-11-07 03:23:18 +0530

    feat: button for navigation, grid styling for kjsce screen

--- src/components/kjsce/KjsceScreen.tsx
+++ src/components/kjsce/KjsceScreen.tsx
@@ -1,12 +1,21 @@
-import { Autocomplete, CircularProgress, TextField } from "@mui/material";
+import {
+  Autocomplete,
+  Button,
+  CircularProgress,
+  Grid,
+  TextField,
+} from "@mui/material";
 import { Box } from "@mui/system";
 import React, { useEffect } from "react";
 import { useState } from "react";
 import calculatorStructure from "../../data/calculatorStructure";
+import { useNavigate } from "react-router-dom";
 
 type Props = {};
 
 const KjsceScreen = (props: Props) => {
+  let navigate = useNavigate();
+
   const [openBranches, setOpenBranches] = useState(false);
   const [allBranches, setAllBranches] = useState<string[]>([]);
   const [selectedBranch, setSelectedBranch] = useState<string>("");
@@ -40,6 +49,14 @@ const KjsceScreen = (props: Props) => {
     }
   }, [selectedBranch]);
 
+  const onPressOpenCalculator = () => {
+    navigate(
+      `/kjsce/${selectedBranch.toLowerCase()}/sem${
+        selectedSemester.split(" ")[1]
+      }/calculator`
+    );
+  };
+
   const getAllBranches = () => {
     if (allBranches.length < 1) {
       const branches = Object.keys(calculatorStructure).map((key) =>
@@ -56,13 +73,14 @@ const KjsceScreen = (props: Props) => {
     val: string | null
   ) => {
     if (!val) return;
+    setOpenBranches(false);
     setSelectedBranch(val);
   };
   const onOpenBranch = () => {
     setOpenBranches(true);
   };
   const onCloseBranch = () => {
-    setOpenBranches(true);
+    setOpenBranches(false);
   };
 
   const getAllSemesters = (branch: string) => {
@@ -85,13 +103,14 @@ const KjsceScreen = (props: Props) => {
     val: string | null
   ) => {
     if (!val) return;
+    setOpenSemesters(false);
     setSelectedSemester(val);
   };
   const onOpenSemester = () => {
     setOpenSemesters(true);
   };
   const onCloseSemester = () => {
-    setOpenSemesters(true);
+    setOpenSemesters(false);
   };
 
   return (
@@ -101,69 +120,94 @@ const KjsceScreen = (props: Props) => {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        px: 2,
       }}
     >
-      {/* branch */}
-      <Autocomplete
-        sx={{ width: 200 }}
-        open={openBranches}
-        value={selectedBranch}
-        autoSelect
-        onChange={onChangeBranch}
-        onOpen={onOpenBranch}
-        onClose={onCloseBranch}
-        options={allBranches}
-        loading={branchLoadingStatus === "loading"}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            label="Branch"
-            InputProps={{
-              ...params.InputProps,
-              endAdornment: (
-                <React.Fragment>
-                  {branchLoadingStatus === "loading" ? (
-                    <CircularProgress color="inherit" size={20} />
-                  ) : null}
-                  {params.InputProps.endAdornment}
-                </React.Fragment>
-              ),
-              style: { textTransform: "uppercase" },
-            }}
-          />
-        )}
-      />
+      <Box sx={{ minWidth: "50vw" }}>
+        <Grid container spacing={2}>
+          {/* branch */}
+          <Grid item xs={12} md={6}>
+            <Autocomplete
+              fullWidth
+              open={openBranches}
+              value={selectedBranch}
+              autoSelect
+              onChange={onChangeBranch}
+              onOpen={onOpenBranch}
+              onClose={onCloseBranch}
+              options={allBranches}
+              loading={branchLoadingStatus === "loading"}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Branch"
+                  InputProps={{
+                    ...params.InputProps,
+                    endAdornment: (
+                      <React.Fragment>
+                        {branchLoadingStatus === "loading" ? (
+                          <CircularProgress color="inherit" size={20} />
+                        ) : null}
+                        {params.InputProps.endAdornment}
+                      </React.Fragment>
+                    ),
+                    style: { textTransform: "uppercase" },
+                  }}
+                />
+              )}
+            />
+          </Grid>
 
-      {/* semester */}
-      <Autocomplete
-        sx={{ width: 200 }}
-        open={openSemesters}
-        value={selectedSemester}
-        autoSelect
-        onChange={onChangeSemester}
-        onOpen={onOpenSemester}
-        onClose={onCloseSemester}
-        options={allSemesters}
-        loading={semesterLoadingStatus === "loading"}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            label="Semester"
-            InputProps={{
-              ...params.InputProps,
-              endAdornment: (
-                <React.Fragment>
-                  {semesterLoadingStatus === "loading" ? (
-                    <CircularProgress color="inherit" size={20} />
-                  ) : null}
-                  {params.InputProps.endAdornment}
-                </React.Fragment>
-              ),
-              style: { textTransform: "uppercase" },
-            }}
-          />
-        )}
-      />
+          {/* semester */}
+          <Grid item xs={12} md={6}>
+            <Autocomplete
+              fullWidth
+              open={openSemesters}
+              value={selectedSemester}
+              autoSelect
+              onChange={onChangeSemester}
+              onOpen={onOpenSemester}
+              onClose={onCloseSemester}
+              options={allSemesters}
+              loading={semesterLoadingStatus === "loading"}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Semester"
+                  InputProps={{
+                    ...params.InputProps,
+                    endAdornment: (
+                      <React.Fragment>
+                        {semesterLoadingStatus === "loading" ? (
+                          <CircularProgress color="inherit" size={20} />
+                        ) : null}
+                        {params.InputProps.endAdornment}
+                      </React.Fragment>
+                    ),
+                    style: { textTransform: "uppercase" },
+                  }}
+                />
+              )}
+            />
+          </Grid>
+
+          {/* button */}
+          <Grid item xs={12}>
+            <Button
+              disabled={
+                selectedBranch.length === 0 || selectedSemester.length === 0
+              }
+              onClick={onPressOpenCalculator}
+              variant="contained"
+              color="success"
+              size="large"
+              fullWidth
+            >
+              Open
+            </Button>
+          </Grid>
+        </Grid>
+      </Box>
     </Box>
   );
 };
