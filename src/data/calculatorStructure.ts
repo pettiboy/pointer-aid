7d40870d9850a325fd03666d3d37f7f86c06ce23
Hussain Pettiwala <64355243+pettiboy@users.noreply.github.com>
2022-11-14 22:46:26 +0530

    feat: add ETRX sem3 and sem4 (#5)
    * feat:Add EXCP,EXTC,MECH
    
    * feat:Add ETRX sem3 and sem4
    
    Co-authored-by: Killjoy <aryaajitnair@gmail.com>

--- src/data/calculatorStructure.ts
+++ src/data/calculatorStructure.ts
@@ -1,6 +1,281 @@
-import { kjsce_groupc_sem1, kjsce_groupc_sem2, kjsce_groupp_sem1 } from "./shared";
+import {
+  kjsce_groupc_sem1,
+  kjsce_groupc_sem2,
+  kjsce_groupp_sem1,
+  kjsce_groupp_sem2,
+} from "./shared";
 
 const calculatorStructure: Record<string, PointerCalculatorStructureType[]> = {
+  // COMPS
+  kjsce_comps_sem1: kjsce_groupc_sem1,
+  kjsce_comps_sem2: kjsce_groupc_sem2,
+  kjsce_comps_sem3: [
+    {
+      subjectName: "Integral Transform and Vector Calculus",
+      subjectCode: "ITVC",
+      creditDistribution: "301",
+    },
+    {
+      subjectName: "Data Structures",
+      subjectCode: "DS",
+      creditDistribution: "300",
+    },
+    {
+      subjectName: "Computrer Organization and Architecture",
+      subjectCode: "COA",
+      creditDistribution: "300",
+    },
+    {
+      subjectName: "Object Oriented Programming Methodology",
+      subjectCode: "OOPM",
+      creditDistribution: "102",
+    },
+    {
+      subjectName: "Discrete Mathematics",
+      subjectCode: "DM",
+      creditDistribution: "301",
+    },
+    {
+      subjectName: "Digital Design Laboratory",
+      subjectCode: "DDL",
+      creditDistribution: "110",
+      maxMarks: 75,
+    },
+    {
+      subjectName: "Data Structures Laboratory",
+      subjectCode: "DSL",
+      creditDistribution: "010",
+    },
+    {
+      subjectName: "Computer Organization & Architecture Laboratory",
+      subjectCode: "COAL",
+      creditDistribution: "010",
+    },
+    {
+      subjectName: "Object Oriented Programming Methodology Laboratory",
+      subjectCode: "OOPML",
+      creditDistribution: "010",
+    },
+  ],
+  kjsce_comps_sem4: [
+    {
+      subjectName: "Probability, Statistics and Optimization Techniques",
+      subjectCode: "PSOT",
+      creditDistribution: "301",
+    },
+    {
+      subjectName: "Analysis of Algorithms",
+      subjectCode: "AOA",
+      creditDistribution: "300",
+    },
+    {
+      subjectName: "Relational Database Management Systems",
+      subjectCode: "RDBMS",
+      creditDistribution: "300",
+    },
+    {
+      subjectName: "Theory of Automata with Compiler Design",
+      subjectCode: "TACD",
+      creditDistribution: "301",
+    },
+    {
+      subjectName: "Web Programming Laboratory",
+      subjectCode: "WPL",
+      creditDistribution: "020",
+      maxMarks: 100,
+    },
+    {
+      subjectName: "Analysis of Algorithms Laboratory ",
+      subjectCode: "AOAL",
+      creditDistribution: "010",
+    },
+    {
+      subjectName: "Relational Database Management Systems Laboratory ",
+      subjectCode: "RDBMSL",
+      creditDistribution: "010",
+    },
+    {
+      subjectName: "Mini Project ",
+      subjectCode: "MP",
+      creditDistribution: "030",
+    },
+  ],
+  kjsce_comps_sem5: [
+    // 300
+    {
+      subjectName: "Software Engineering",
+      subjectCode: "SE",
+      creditDistribution: "300",
+    },
+    {
+      subjectName: "Computer Networks",
+      subjectCode: "CN",
+      creditDistribution: "300",
+    },
+    {
+      subjectName: "Operating System",
+      subjectCode: "OS",
+      creditDistribution: "300",
+    },
+    {
+      subjectName: "Departmental Elective-I",
+      subjectCode: "DEI",
+      creditDistribution: "300",
+    },
+
+    // 010
+    {
+      subjectName: "Software Engineering Lab",
+      subjectCode: "SEL",
+      creditDistribution: "010",
+    },
+    {
+      subjectName: "Computer Networks Lab",
+      subjectCode: "CNL",
+      creditDistribution: "010",
+    },
+    {
+      subjectName: "Operating System Lab",
+      subjectCode: "OSL",
+      creditDistribution: "010",
+    },
+    {
+      subjectName: "Departmental Elective-I Lab",
+      subjectCode: "DEIL",
+      creditDistribution: "010",
+    },
+
+    // 200
+    {
+      subjectName: "OET",
+      subjectCode: "OET",
+      creditDistribution: "200",
+    },
+    {
+      subjectName: "OEHM",
+      subjectCode: "OEHM",
+      creditDistribution: "200",
+    },
+
+    // 110
+    {
+      subjectName: "Full Stack Development Lab",
+      subjectCode: "FSDL",
+      creditDistribution: "110",
+      maxMarks: 50,
+    },
+  ],
+
+  // ETRX
+  kjsce_etrx_sem3: [
+    {
+      subjectName: "Mathematics for Electronics Engineering-I",
+      subjectCode: "DAM",
+      creditDistribution: "301",
+    },
+    {
+      subjectName: "Electrical Networks",
+      subjectCode: "EN",
+      creditDistribution: "301",
+    },
+    {
+      subjectName: "Basic of Electronics Circuit",
+      subjectCode: "BEC",
+      creditDistribution: "300",
+    },
+    {
+      subjectName: "Digital Electronics",
+      subjectCode: "DE",
+      creditDistribution: "300",
+    },
+    {
+      subjectName: "Signals and Systems",
+      subjectCode: "SAS",
+      creditDistribution: "300",
+    },
+    {
+      subjectName: "Programming Laboratory",
+      subjectCode: "PL",
+      creditDistribution: "011",
+      //used 011 instead of 020 to match the 50+25 marks pattern
+    },
+    {
+      subjectName: "Basic of Electronics Circuits Laboratory",
+      subjectCode: "BECL",
+      creditDistribution: "010",
+    },
+    {
+      subjectName: "Digital Electronics Laboratory",
+      subjectCode: "DEL",
+      creditDistribution: "010",
+    },
+    {
+      subjectName: "Signals and Systems Laboratory",
+      subjectCode: "SASL",
+      creditDistribution: "010",
+    },
+  ],
+  kjsce_etrx_sem4: [
+    {
+      subjectName: "Mathematics for Electronics Engineering-II",
+      subjectCode: "MEE",
+      creditDistribution: "301",
+    },
+    {
+      subjectName: "Analog Electronics Circuits",
+      subjectCode: "AEC",
+      creditDistribution: "300",
+    },
+    {
+      subjectName: "Control System Engineering ",
+      subjectCode: "CSE",
+      creditDistribution: "300",
+    },
+    {
+      subjectName: "Analog and Digital Communication",
+      subjectCode: "ADC",
+      creditDistribution: "300",
+    },
+    {
+      subjectName: "Microcontroller and Applications",
+      subjectCode: "MAC",
+      creditDistribution: "300",
+    },
+    {
+      subjectName: "Designing with Programmable Logic Lab Course",
+      subjectCode: "DEPLL",
+      creditDistribution: "011",
+    },
+    {
+      subjectName: "Analog Electronics Circuits Laboratory",
+      subjectCode: "AECL",
+      creditDistribution: "010",
+    },
+    {
+      subjectName: "Control System Engineering Laboratory",
+      subjectCode: "CSEL",
+      creditDistribution: "010",
+    },
+    {
+      subjectName: "Analog and Digital Communication Laboratory",
+      subjectCode: "ADCL",
+      creditDistribution: "010",
+    },
+    {
+      subjectName: "Microcontroller and Applications Laboratory",
+      subjectCode: "MAL",
+      creditDistribution: "010",
+    },
+  ],
+
+  // EXCP
+  kjsce_excp_sem1: kjsce_groupp_sem1,
+  kjsce_excp_sem2: kjsce_groupp_sem2,
+
+  // EXTC
+  kjsce_extc_sem1: kjsce_groupp_sem1,
+  kjsce_extc_sem2: kjsce_groupp_sem2,
+
   // IT
   kjsce_it_sem1: kjsce_groupc_sem1,
   kjsce_it_sem2: kjsce_groupc_sem2,
@@ -168,178 +443,9 @@ const calculatorStructure: Record<string, PointerCalculatorStructureType[]> = {
     },
   ],
 
-  // COMPS
-  kjsce_comps_sem1: kjsce_groupc_sem1,
-  kjsce_comps_sem2: kjsce_groupc_sem2,
-  kjsce_comps_sem3: [
-    {
-      subjectName: "Integral Transform and Vector Calculus",
-      subjectCode: "ITVC",
-      creditDistribution: "301",
-    },
-    {
-      subjectName: "Data Structures",
-      subjectCode: "DS",
-      creditDistribution: "300",
-    },
-    {
-      subjectName: "Computrer Organization and Architecture",
-      subjectCode: "COA",
-      creditDistribution: "300",
-    },
-    {
-      subjectName: "Object Oriented Programming Methodology",
-      subjectCode: "OOPM",
-      creditDistribution: "102",
-    },
-    {
-      subjectName: "Discrete Mathematics",
-      subjectCode: "DM",
-      creditDistribution: "301",
-    },
-    {
-      subjectName: "Digital Design Laboratory",
-      subjectCode: "DDL",
-      creditDistribution: "110",
-      maxMarks: 75,
-    },
-    {
-      subjectName: "Data Structures Laboratory",
-      subjectCode: "DSL",
-      creditDistribution: "010",
-    },
-    {
-      subjectName: "Computer Organization & Architecture Laboratory",
-      subjectCode: "COAL",
-      creditDistribution: "010",
-    },
-    {
-      subjectName: "Object Oriented Programming Methodology Laboratory",
-      subjectCode: "OOPML",
-      creditDistribution: "010",
-    },
-  ],
-  kjsce_comps_sem4: [
-    {
-      subjectName: "Probability, Statistics and Optimization Techniques",
-      subjectCode: "PSOT",
-      creditDistribution: "301",
-    },
-    {
-      subjectName: "Analysis of Algorithms",
-      subjectCode: "AOA",
-      creditDistribution: "300",
-    },
-    {
-      subjectName: "Relational Database Management Systems",
-      subjectCode: "RDBMS",
-      creditDistribution: "300",
-    },
-    {
-      subjectName: "Theory of Automata with Compiler Design",
-      subjectCode: "TACD",
-      creditDistribution: "301",
-    },
-    {
-      subjectName: "Web Programming Laboratory",
-      subjectCode: "WPL",
-      creditDistribution: "020",
-      maxMarks: 100,
-    },
-    {
-      subjectName: "Analysis of Algorithms Laboratory ",
-      subjectCode: "AOAL",
-      creditDistribution: "010",
-    },
-    {
-      subjectName: "Relational Database Management Systems Laboratory ",
-      subjectCode: "RDBMSL",
-      creditDistribution: "010",
-    },
-    {
-      subjectName: "Mini Project ",
-      subjectCode: "MP",
-      creditDistribution: "030",
-    },
-  ],
-  kjsce_comps_sem5: [
-    // 300
-    {
-      subjectName: "Software Engineering",
-      subjectCode: "SE",
-      creditDistribution: "300",
-    },
-    {
-      subjectName: "Computer Networks",
-      subjectCode: "CN",
-      creditDistribution: "300",
-    },
-    {
-      subjectName: "Operating System",
-      subjectCode: "OS",
-      creditDistribution: "300",
-    },
-    {
-      subjectName: "Departmental Elective-I",
-      subjectCode: "DEI",
-      creditDistribution: "300",
-    },
-
-    // 010
-    {
-      subjectName: "Software Engineering Lab",
-      subjectCode: "SEL",
-      creditDistribution: "010",
-    },
-    {
-      subjectName: "Computer Networks Lab",
-      subjectCode: "CNL",
-      creditDistribution: "010",
-    },
-    {
-      subjectName: "Operating System Lab",
-      subjectCode: "OSL",
-      creditDistribution: "010",
-    },
-    {
-      subjectName: "Departmental Elective-I Lab",
-      subjectCode: "DEIL",
-      creditDistribution: "010",
-    },
-
-    // 200
-    {
-      subjectName: "OET",
-      subjectCode: "OET",
-      creditDistribution: "200",
-    },
-    {
-      subjectName: "OEHM",
-      subjectCode: "OEHM",
-      creditDistribution: "200",
-    },
-
-    // 110
-    {
-      subjectName: "Full Stack Development Lab",
-      subjectCode: "FSDL",
-      creditDistribution: "110",
-      maxMarks: 50,
-    },
-  ],
-
-  //EXTC
-  kjsce_extc_sem1:kjsce_groupp_sem1,
-  kjsce_extc_sem2:kjsce_groupc_sem2,
-
-  //EXCP
-  kjsce_excp_sem1:kjsce_groupp_sem1,
-  kjsce_excp_sem2:kjsce_groupc_sem2,
-
-  //MECH 
-  kjsce_mech_sem1:kjsce_groupp_sem1,
-  kjsce_mech_sem2:kjsce_groupc_sem2,
-
+  // MECH
+  kjsce_mech_sem1: kjsce_groupp_sem1,
+  kjsce_mech_sem2: kjsce_groupp_sem2,
 };
 
 export default calculatorStructure;
