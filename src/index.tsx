66011f260be371448412ab504c51f7806f7653b6
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2023-01-17 01:00:15 +0530

    feat: implement skipWaiting to update PWA

--- src/index.tsx
+++ src/index.tsx
@@ -23,7 +23,17 @@ root.render(
   </React.StrictMode>
 );
 
-serviceWorkerRegistration.register();
+serviceWorkerRegistration.register({
+  onUpdate: (e) => {
+    const { waiting: { postMessage = null } = {} as any, update } = e || {};
+    if (postMessage) {
+      postMessage({ type: "SKIP_WAITING" });
+    }
+    update().then(() => {
+      window.location.reload();
+    });
+  },
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
