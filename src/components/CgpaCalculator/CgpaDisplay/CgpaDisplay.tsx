bb6f6520d3f2008a93a0f068046aa6e5396fa35d
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2023-06-03 16:37:01 +0530

    feat: ui for cgpa input and display with sync button

---
+++ src/components/CgpaCalculator/CgpaDisplay/CgpaDisplay.tsx
@@ -0,0 +1,109 @@
+import { Box, Button, Paper, TextField, Typography } from "@mui/material";
+import { useContext, useEffect, useState } from "react";
+import useDetectKeyboardOpen from "use-detect-keyboard-open";
+import { CgpaCalculatorContext } from "../../../context/CgpaCalculatorContext";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import useWindowDimensions from "../../../hooks/useWindowDimentions";
+
+type Props = {};
+
+const CgpaDisplay = (props: Props) => {
+  const isKeyboardOpen = useDetectKeyboardOpen();
+  const { width } = useWindowDimensions();
+
+  const { calculateCurrentAverage, refreshAverageCount } = useContext(
+    CgpaCalculatorContext
+  );
+
+  const [currentAverage, setCurrentAverage] = useState<number>(
+    calculateCurrentAverage()
+  );
+
+  const [cgpaInputState, setCgpaInputState] = useState<string>("");
+
+  const [isCgpaInputFocused, setIsCgpaInputFocused] = useState(false);
+
+  useEffect(() => {
+    const updatedCurrentAverage = calculateCurrentAverage();
+
+    setCurrentAverage(updatedCurrentAverage);
+    setCgpaInputState(updatedCurrentAverage.toString());
+  }, [refreshAverageCount]);
+
+  // input on change function
+  const onChangeCgpaInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setCgpaInputState(value);
+  };
+
+  const handleFocus = () => {
+    setIsCgpaInputFocused(true);
+  };
+
+  const handleBlur = () => {
+    setIsCgpaInputFocused(false);
+  };
+
+  const showEqualizer = cgpaInputState !== currentAverage.toString();
+
+  const spacingL = !showEqualizer && width > 600 ? 3 : 2;
+  const spacingM = !showEqualizer && width > 600 ? 2 : 1;
+
+  return (
+    <>
+      {(!isKeyboardOpen || isCgpaInputFocused) && (
+        <Paper
+          sx={{
+            position: "fixed",
+            bottom: 0,
+            left: 0,
+            width: "100vw",
+            px: spacingL,
+            py: 2,
+            zIndex: 9,
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <Typography
+                variant={showEqualizer && width < 600 ? "h5" : "h4"}
+                sx={{ mr: spacingM }}
+              >
+                CGPA:
+              </Typography>
+              <TextField
+                value={cgpaInputState}
+                onChange={onChangeCgpaInput}
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+              />
+              {showEqualizer && (
+                <>
+                  <Button
+                    variant="outlined"
+                    endIcon={<ArrowForwardIcon />}
+                    sx={{ ml: spacingM }}
+                  >
+                    {"sync"}
+                  </Button>
+                  <Typography sx={{ ml: spacingM }} variant="h3">
+                    {currentAverage}
+                  </Typography>
+                </>
+              )}
+            </Box>
+          </Box>
+        </Paper>
+      )}
+    </>
+  );
+};
+
+export default CgpaDisplay;
--- src/screens/CgpaCalculator/CgpaCalculator.tsx
+++ src/screens/CgpaCalculator/CgpaCalculator.tsx
@@ -4,34 +4,23 @@ import {
   Unstable_Grid2 as Grid,
   Typography,
 } from "@mui/material";
-import { useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Helmet } from "react-helmet-async";
 import { useParams } from "react-router-dom";
 import SgpaContainer from "../../components/CgpaCalculator/SgpaContainer/SgpaContainer";
-import { CgpaCalculatorContext } from "../../context/CgpaCalculatorContext";
+import CgpaDisplay from "../../components/CgpaCalculator/CgpaDisplay/CgpaDisplay";
+import Credits from "../../components/Credits/Credits";
 
 type Props = {};
 
 const CgpaCalculator = (props: Props) => {
   const { college, branch } = useParams();
 
-  const { calculateCurrentAverage, refreshAverageCount } = useContext(
-    CgpaCalculatorContext
-  );
-
   const [loadingStatus, setLoadingStatus] = useState<StatusType>("loading");
-
-  const [currentAverage, setCurrentAverage] = useState<number>(
-    calculateCurrentAverage()
-  );
 
   useEffect(() => {
     setLoadingStatus("loaded");
   }, []);
-
-  useEffect(() => {
-    setCurrentAverage(calculateCurrentAverage());
-  }, [refreshAverageCount]);
 
   return (
     <Box sx={{ p: 3 }}>
@@ -58,10 +47,17 @@ const CgpaCalculator = (props: Props) => {
             <Grid xs={12} md={6} lg={6} xl={4}>
               <SgpaContainer id="2" title="Semester 2" weightage={32} />
             </Grid>
+            <Grid xs={12} md={6} lg={6} xl={4}>
+              <SgpaContainer id="3" title="Semester 3" weightage={22} />
+            </Grid>
+
+            <Credits styles={{ textAlign: "center", mb: 10, mt: 3 }} />
           </Grid>
         </>
       )}
-      <Typography>CGPA: {currentAverage}</Typography>
+
+      <CgpaDisplay />
+
       {loadingStatus === "no_data" && (
         <Box className="h-85 flex-center">
           <Typography variant="h1">No calculator available yet</Typography>
