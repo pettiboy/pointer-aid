3aabd22f31cc6177539977b879eb608c448d2f2e
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2023-06-05 14:27:35 +0530

    feat: update UI for CgpaDisplay component

--- src/components/CgpaCalculator/CgpaDisplay/CgpaDisplay.tsx
+++ src/components/CgpaCalculator/CgpaDisplay/CgpaDisplay.tsx
@@ -1,26 +1,22 @@
-import { Box, Button, Paper, TextField, Typography } from "@mui/material";
+import { Box, Paper, Typography, useTheme } from "@mui/material";
 import { useContext, useEffect, useState } from "react";
 import useDetectKeyboardOpen from "use-detect-keyboard-open";
 import { CgpaCalculatorContext } from "../../../context/CgpaCalculatorContext";
-import SyncAltIcon from "@mui/icons-material/SyncAlt";
-import useWindowDimensions from "../../../hooks/useWindowDimentions";
-import round from "../../../utils/round";
 
 type Props = {};
 
-// Todo: input validation
-// Todo: for large screens align to side
+// Todo(responsiveness): for large screens make display of CGPA
+//      label and value side by side for both containers
 
 const CgpaDisplay = (props: Props) => {
   const isKeyboardOpen = useDetectKeyboardOpen();
-  const { width } = useWindowDimensions();
+  const theme = useTheme();
 
   const {
     calculateCurrentAverage,
     calculateCurrentAverageForLocked,
     refreshAverageCount,
     refreshLockedAverageCount,
-    calculateAndRefreshPredictions,
   } = useContext(CgpaCalculatorContext);
 
   const [currentAverage, setCurrentAverage] = useState<number>(
@@ -29,15 +25,10 @@ const CgpaDisplay = (props: Props) => {
 
   const [currentLockedAverage, setCurrentLockedAverage] = useState<number>(0);
 
-  const [cgpaInputState, setCgpaInputState] = useState<string>("");
-
-  const [isCgpaInputFocused, setIsCgpaInputFocused] = useState(false);
-
   useEffect(() => {
     const updatedCurrentAverage = calculateCurrentAverage();
 
     setCurrentAverage(updatedCurrentAverage);
-    setCgpaInputState(updatedCurrentAverage.toString());
   }, [refreshAverageCount]);
 
   useEffect(() => {
@@ -46,36 +37,16 @@ const CgpaDisplay = (props: Props) => {
     setCurrentLockedAverage(updatedCurrentLockedAverage);
   }, [refreshLockedAverageCount]);
 
-  // input on change function
-  const onChangeCgpaInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    setCgpaInputState(value);
-  };
-
-  const handleFocus = () => {
-    setIsCgpaInputFocused(true);
-  };
-
-  const handleBlur = () => {
-    setIsCgpaInputFocused(false);
-  };
-
-  const showEqualizer =
-    parseFloat(cgpaInputState).toString() !== currentAverage.toString();
-
-  const spacingL = !showEqualizer && width > 600 ? 3 : 2;
-  const spacingM = !showEqualizer && width > 600 ? 2 : 1;
-
   return (
     <>
-      {(!isKeyboardOpen || isCgpaInputFocused) && (
+      {!isKeyboardOpen && (
         <Paper
           sx={{
             position: "fixed",
             bottom: 0,
             left: 0,
             width: "100vw",
-            px: spacingL,
+            px: 2,
             py: 2,
             zIndex: 9,
           }}
@@ -88,66 +59,31 @@ const CgpaDisplay = (props: Props) => {
               alignItems: "center",
             }}
           >
+            {/* container for overall CGPA (all semesters considered) */}
             <Box
               sx={{
-                display: "flex",
-                alignItems: "center",
+                flex: 1,
               }}
             >
-              {/* <Typography
-                variant={showEqualizer && width < 600 ? "h5" : "h4"}
-                sx={{ mr: spacingM }}
-              >
-                CGPA:
-              </Typography> */}
-
-              <Box>
-                {showEqualizer && (
-                  <Typography variant="subtitle2">Target CGPA</Typography>
-                )}
-                <TextField
-                  value={cgpaInputState}
-                  onChange={onChangeCgpaInput}
-                  onFocus={handleFocus}
-                  onBlur={handleBlur}
-                  inputProps={{
-                    inputMode: "decimal",
-                    style: showEqualizer
-                      ? {
-                          padding: 8,
-                        }
-                      : {},
-                  }}
-                />
-              </Box>
-
-              {showEqualizer && (
-                <>
-                  <Button
-                    variant="outlined"
-                    endIcon={<SyncAltIcon />}
-                    sx={{ ml: spacingM, minWidth: 100, alignSelf: "stretch" }}
-                    onClick={() => {
-                      calculateAndRefreshPredictions(
-                        round(parseFloat(cgpaInputState), 2)
-                      );
-                    }}
-                  >
-                    {"sync"}
-                  </Button>
-                  <Box sx={{ ml: spacingM }}>
-                    <Typography variant="subtitle2">Current</Typography>
-                    <Typography sx={{}} variant="h4">
-                      {currentAverage}
-                    </Typography>
-                  </Box>
-                </>
-              )}
+              <Typography>Overall CGPA</Typography>
+              <Typography sx={{}} variant="h4">
+                {currentAverage}
+              </Typography>
             </Box>
 
-            <Typography sx={{}} variant="h4">
-              {currentLockedAverage.toString()}
-            </Typography>
+            {/* container for current CGPA (only locked semesters considered) */}
+            <Box
+              sx={{
+                flex: 1,
+                borderLeft: "1px solid " + theme.palette.text.primary,
+                pl: 2,
+              }}
+            >
+              <Typography>Current CGPA</Typography>
+              <Typography sx={{}} variant="h4">
+                {currentLockedAverage.toString()}
+              </Typography>
+            </Box>
           </Box>
         </Paper>
       )}
